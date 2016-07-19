4adf1173711833d7a1514f48f9f96e5deae27d05
ilabafrica <ilabafrica@ilabafricas-iMac-10.local>
2016-07-19 18:52:53 +0300

    Initial

---
+++ Code/MechApp/MechPal/MechPal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MechPal
+//
+//  Created by ilabafrica on 7/19/16.
+//  Copyright (c) 2016 ilabafrica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
