80c2176754a14ae2abbc0460103a622d0c475114
KAV525A <mwendwa.nancieh@gmail.com>
2016-08-03 16:49:31 +0300

    Nancy first code commit, UI

---
+++ Code/MechPal/MechPal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MechPal
+//
+//  Created by ilabadmin on 8/3/16.
+//  Copyright © 2016 ilabafrica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Code/MechPal/MechPal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MechPal
+//
+//  Created by ilabadmin on 8/3/16.
+//  Copyright © 2016 ilabafrica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
